ae06e9509da4e4b68a1c0485624461403fd2802e
chesnokoff <chesnokoff239@gmail.com>
2025-07-08 11:22:08 +0300

    IGNITE-25710 Add test for IgniteScalaAbbreviationInspection

---
+++ src/test/resources/inspection/Abbreviation1.scala
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class Abbreviation {
+  def foo(): Unit = {
+    val address<caret> : Int = 0
+    val affinity : Int = 0
+    val argument : Int = 0
+    val array : Int = 0
+    val attribute : Int = 0
+    val attributes : Int = 0
+    val buffer : Int = 0
+    val command : Int = 0
+    val config : Int = 0
+    val context : Int = 0
+    val count : Int = 0
+    val current : Int = 0
+    val database : Int = 0
+    val directory : Int = 0
+    val event : Int = 0
+    val exception : Int = 0
+    val execute : Int = 0
+    val frequency : Int = 0
+    val future : Int = 0
+    val group : Int = 0
+    val handler : Int = 0
+    val header : Int = 0
+    val implementation : Int = 0
+    val index : Int = 0
+    val initial1 : Int = 0
+    val initialize2 : Int = 0
+    val iterator : Int = 0
+    val label : Int = 0
+    val local : Int = 0
+    val logger : Int = 0
+    val loader : Int = 0
+    val manager : Int = 0
+    val message : Int = 0
+    val mutex : Int = 0
+    val network : Int = 0
+    val object1 : Int = 0
+    val parameter : Int = 0
+    val password : Int = 0
+    val policy : Int = 0
+    val property : Int = 0
+    val properties : Int = 0
+    val process1 : Int = 0
+    val processor2 : Int = 0
+    val query : Int = 0
+    val receive : Int = 0
+    val reference : Int = 0
+    val repository : Int = 0
+    val request : Int = 0
+    val resource : Int = 0
+    val response : Int = 0
+    val sender : Int = 0
+    val service : Int = 0
+    val session : Int = 0
+    val sequence : Int = 0
+    val socket : Int = 0
+    val source : Int = 0
+    val string : Int = 0
+    val subject : Int = 0
+    val system : Int = 0
+    val topology : Int = 0
+    val value: Int = 0
+    val version : Int = 0
+  }
+}
