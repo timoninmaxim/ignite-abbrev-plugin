80b57be742947b20d7ee682713170c86b3d00888
chesnokoff <chesnokoff239@gmail.com>
2025-07-15 10:12:12 +0300

    IGNITE-25710 Update code of IgniteScalaAbbreviationInspection.java

--- src/main/java/org/apache/ignite/idea/inspection/abbrev/IgniteScalaAbbreviationInspection.java
+++ src/main/java/org/apache/ignite/idea/inspection/abbrev/IgniteScalaAbbreviationInspection.java
@@ -15,98 +15,159 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.idea.inspection.abbrev
+package org.apache.ignite.idea.inspection.abbrev;
 
-import com.intellij.codeInspection._
-import com.intellij.openapi.components.ServiceManager
-import com.intellij.psi.{PsiElement, PsiElementVisitor, PsiIdentifier, PsiNamedElement}
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaElementVisitor
-import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScValue, ScValueDeclaration, ScVariableDefinition}
-import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
+import com.intellij.codeInspection.*;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiIdentifier;
+import com.intellij.psi.PsiNamedElement;
+import com.intellij.refactoring.JavaRefactoringFactory;
+import com.intellij.refactoring.RenameRefactoring;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaElementVisitor;
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScValue;
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScValueDeclaration;
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter;
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScVariableDefinition;
+import org.apache.ignite.idea.util.IgniteUtils;
 
-import scala.collection.JavaConversions._
-import com.intellij.openapi.project.Project
-import com.intellij.refactoring.{JavaRefactoringFactory, RenameRefactoring}
-import org.apache.ignite.idea.util.IgniteUtils
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
-/**
- * Abbreviation inspection for Scala language.
- */
-class IgniteScalaAbbreviationInspection extends LocalInspectionTool {
+/** Inspects usages of Ignite abbreviations. */
+public class IgniteScalaAbbreviationInspection extends LocalInspectionTool {
     /** Abbreviation exceptions. */
-    val abbrExceptions = Set("value")
+    private final Set<String> abbrExceptions = Set.of("value");
 
-    override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean,
-        ses: LocalInspectionToolSession): PsiElementVisitor = {
-        new ScalaElementVisitor {
-            override def visitValueDeclaration(v: ScValueDeclaration) =
-                v.declaredElementsArray.foreach(checkShouldAbbreviate)
+    /** {@inheritDoc} */
+    @Override public PsiElementVisitor buildVisitor(ProblemsHolder holder, boolean isOnTheFly, LocalInspectionToolSession ses) {
+        return new ScalaElementVisitor() {
+            /** {@inheritDoc} */
+            @Override public void visitValueDeclaration(ScValueDeclaration v) {
+                Arrays.stream(v.declaredElementsArray()).forEach(this::checkShouldAbbreviate);
+            }
 
-            override def visitValue(v: ScValue) =
-                v.declaredElementsArray.foreach(checkShouldAbbreviate)
+            /** {@inheritDoc} */
+            @Override public void visitValue(ScValue v) {
+                Arrays.stream(v.declaredElementsArray()).forEach(this::checkShouldAbbreviate);
+            }
 
-            override def visitParameter(p: ScParameter) =
-                checkShouldAbbreviate(p, p.getNameIdentifier())
+            /** {@inheritDoc} */
+            @Override public void visitParameter(ScParameter p) {
+                checkShouldAbbreviate(p, p.getNameIdentifier());
+            }
 
-            override def visitVariableDefinition(v: ScVariableDefinition) =
-                v.declaredElementsArray.foreach(checkShouldAbbreviate)
+            /** {@inheritDoc} */
+            @Override public void visitVariableDefinition(ScVariableDefinition v) {
+                Arrays.stream(v.declaredElementsArray()).forEach(this::checkShouldAbbreviate);
+            }
 
             /**
              * Checks if given name contains a part that should be abbreviated and registers fix if needed.
              *
              * @param elem Element to check and rename.
              */
-            private def checkShouldAbbreviate(elem: PsiNamedElement) =
-                check0(IgniteUtils.camelCaseParts(elem.getName), elem)
+            private void checkShouldAbbreviate(PsiNamedElement elem) {
+                check0(IgniteUtils.camelCaseParts(elem.getName()), elem);
+            }
 
             /**
              * Checks if given name contains a part that should be abbreviated and registers fix if needed.
              *
              * @param elem Element to check and rename.
              */
-            private def checkShouldAbbreviate(elem: PsiElement, id: PsiIdentifier) =
-                check0(IgniteUtils.camelCaseParts(id.getText), elem)
+            private void checkShouldAbbreviate(PsiElement elem, PsiIdentifier id) {
+                check0(IgniteUtils.camelCaseParts(id.getText()), elem);
+            }
 
             /**
-             * Checks that all identifier parts are correctly abbreviated. Registers problem if
-             * needed.
+             * Checks that all identifier parts are correctly abbreviated. Registers problem if needed.
              *
              * @param parts Identifier parts.
              * @param elem Checked identifier element.
              */
-            private def check0(parts: java.util.List[String], elem: PsiElement): Unit = {
-                for (part <- parts) {
-                    val config: IgniteAbbreviationConfig = ServiceManager.getService(elem.getProject, classOf[IgniteAbbreviationConfig])
+            private void check0(List<String> parts, PsiElement elem) {
+                for (String part : parts) {
+                    IgniteAbbreviationConfig cfg = elem.getProject().getService(IgniteAbbreviationConfig.class);
 
-                    if (!abbrExceptions.contains(part) && config.getAbbreviation(part) != null) {
+                    if (!abbrExceptions.contains(part) && cfg.getAbbreviation(part) != null) {
                         holder.registerProblem(elem, "Abbreviation should be used",
-                            new RenameToFix(config.replaceWithAbbreviations(parts)))
+                            new RenameToFix(cfg.replaceWithAbbreviations(parts)));
 
-                        return
+                        return;
                     }
                 }
             }
-        }
+        };
     }
 
-    /**
-     * Rename quick fix.
-     *
-     * @param name New name for the identifier.
-     */
-    private class RenameToFix(val name: String) extends java.lang.Object with LocalQuickFix {
-        def getName: String = "Rename to " + name
+    /** Rename quick fix. */
+    private static class RenameToFix implements LocalQuickFix, BatchQuickFix {
+        /** Logger. */
+        private static final Logger LOG = Logger.getInstance(RenameToFix.class);
 
-        def getFamilyName: String = ""
+        /** */
+        private final String name;
 
-        def applyFix(proj: Project, descr: ProblemDescriptor) = {
-            val element: PsiElement = descr.getPsiElement
+        /**
+         * @param name New name for the identifier.
+         */
+        public RenameToFix(String name) {
+            this.name = name;
+        }
 
-            val factory: JavaRefactoringFactory = JavaRefactoringFactory.getInstance(proj)
+        /** {@inheritDoc} */
+        @Override public String getName() {
+            return "Rename to " + name;
+        }
 
-            val renRefactoring: RenameRefactoring = factory.createRename(element, name, false, false)
+        /** {@inheritDoc} */
+        @Override public String getFamilyName() {
+            return "Use abbreviation";
+        }
 
-            renRefactoring.run()
+        /** {@inheritDoc} */
+        @Override public boolean startInWriteAction() {
+            return false;
+        }
+
+        /** {@inheritDoc} */
+        @Override public void applyFix(@NotNull Project project, CommonProblemDescriptor[] descriptors,
+            @NotNull List<PsiElement> psiElementsToIgnore, @Nullable Runnable refreshViews) {
+            for (CommonProblemDescriptor descriptor : descriptors) {
+                QuickFix[] fixes = descriptor.getFixes();
+
+                if (fixes == null || fixes.length == 0) {
+                    LOG.warn("No fixes found.");
+                    continue;
+                }
+
+                Optional<QuickFix> renameFix = Arrays.stream(fixes)
+                    .filter(RenameToFix.class::isInstance)
+                    .findAny();
+
+                if (renameFix.isEmpty()) {
+                    LOG.warn("No rename fix found.");
+                    continue;
+                }
+
+                renameFix.get().applyFix(project, descriptor);
+            }
+        }
+
+        /** {@inheritDoc} */
+        @Override public void applyFix(Project proj, ProblemDescriptor descr) {
+            PsiElement element = descr.getPsiElement();
+
+            JavaRefactoringFactory factory = JavaRefactoringFactory.getInstance(proj);
+            RenameRefactoring renRefactoring = factory.createRename(element, name, false, false);
+            renRefactoring.run();
         }
     }
 }
